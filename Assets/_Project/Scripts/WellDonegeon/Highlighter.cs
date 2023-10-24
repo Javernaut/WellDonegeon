4d34073ea45457ec7f15a07f35a3d41eccc8aab0
javernaut <javernaut@gmail.com>
2023-10-24 21:19:15 +0200

    Highlighting multiple parts of an InteractableThing

--- Assets/_Project/Scripts/WellDonegeon/Highlighter.cs
+++ Assets/_Project/Scripts/WellDonegeon/Highlighter.cs
@@ -5,21 +5,24 @@ namespace WellDonegeon
 {
     public class Highlighter : MonoBehaviour
     {
-        [SerializeField] private Renderer renderer;
+        [SerializeField] private Renderer[] renderers;
         [SerializeField] private Color color = Color.white;
 
         public void SetHighlighted(bool selected)
         {
-            var material = renderer.material;
-            if (selected)
+            foreach (var subRender in renderers)
             {
-                material.EnableKeyword("_EMISSION");
-                //before we can set the color
-                material.SetColor("_EmissionColor", color);
-            }
-            else
-            {
-                material.DisableKeyword("_EMISSION");
+                var material = subRender.material;
+                if (selected)
+                {
+                    material.EnableKeyword("_EMISSION");
+                    //before we can set the color
+                    material.SetColor("_EmissionColor", color);
+                }
+                else
+                {
+                    material.DisableKeyword("_EMISSION");
+                }   
             }
         }
     }
